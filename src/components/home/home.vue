d4fa4cf54da6b915eac2a8d8056267d436450f4d
Евгений <skytrash118@gmail.com>
2025-06-23 21:26:25 +0400

    add home balance income trsnferMoney cardSettings

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -2,6 +2,10 @@
 import Cards from '../cards/cards.vue'
 import CardsTwo from '../cards/cards-two.vue'
 import CardsCredit from '../cards/cardsCredit.vue'
+import Balanse from '../total/balanse.vue'
+import TransferMoney from '../button/transferMoney.vue'
+import CardSettings from '../cardSettings/cardSettings.vue'
+import Income from '../total/income.vue'
 
 import { useRouter } from 'vue-router'
 import { signOut } from 'firebase/auth'
@@ -21,6 +25,14 @@ const logout = async () => {
       <Cards />
       <CardsTwo/>
       <CardsCredit />
+    </div>
+    <div class="home__button">
+      <Balanse/>
+      <Income/>
+      <TransferMoney/>
+    </div>
+    <div class="home__settings">
+      <CardSettings/>
     </div>
   </main>
 </template>
