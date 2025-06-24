7bafebce44a4b00b5b492e85be02b39ddb7240d6
Евгений <skytrash118@gmail.com>
2025-06-24 13:02:00 +0400

    fix bug

--- src/components/register/register.vue
+++ src/components/register/register.vue
@@ -40,20 +40,21 @@ const generateCardNumber = () => {
 }
 
 function transliterate(name) {
-  if (!name) return ''
+  if (!name) return '';
+  
   const cyr = {
     'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd',
     'е': 'e', 'ё': 'yo', 'ж': 'zh', 'з': 'z', 'и': 'i',
     'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n',
     'о': 'o', 'п': 'p', 'р': 'r', 'с': 's', 'т': 't',
     'у': 'u', 'ф': 'f', 'х': 'h', 'ц': 'ts', 'ч': 'ch',
-    'ш': 'sh', 'щ': 'sch', 'ъ': '', 'ы': 'y', 'ь': '',
+    'ш': 'sh', 'щ': 'sch', 'ъ': '', 'ы': '', 'ь': '', 
     'э': 'e', 'ю': 'yu', 'я': 'ya'
-  }
+  };
   
   return name.toLowerCase().split('').map(char => 
     cyr[char] || char
-  ).join('')
+  ).join('');
 }
 
 const generateValidThru = () => {
